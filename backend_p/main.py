285461fd13e49983b1677d6cdca1c289a64aed49
SergioA <sergioagreda21@outlook.com>
2025-06-05 11:34:05 -0400

    Changed port to 465 for Siteground smtp

--- backend_p/main.py
+++ backend_p/main.py
@@ -55,10 +55,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
     MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD"),
     MAIL_FROM=os.environ.get("MAIL_FROM"),
-    MAIL_PORT=int(os.environ.get("MAIL_PORT", 587)),
-    MAIL_SERVER=os.environ.get("MAIL_SERVER", "mail.yourdomain.com"),
-    MAIL_STARTTLS=os.environ.get("MAIL_STARTTLS", "True") == "True",
-    MAIL_SSL_TLS=os.environ.get("MAIL_SSL_TLS", "False") == "True",
+    MAIL_PORT=int(os.environ.get("MAIL_PORT", 465)),
+    MAIL_SERVER=os.environ.get("MAIL_SERVER", "mail.parlamento.com.bo"),
+    MAIL_STARTTLS=os.environ.get("MAIL_STARTTLS", "False") == "True",
+    MAIL_SSL_TLS=os.environ.get("MAIL_SSL_TLS", "True") == "True",
     USE_CREDENTIALS=os.environ.get("USE_CREDENTIALS", "True") == "True"
 )
 
@@ -517,7 +517,7 @@ async def book_event_email(data: dict, background_tasks: BackgroundTasks):
 
     message = MessageSchema(
         subject="Nueva reserva de evento desde la web",
-        recipients=["sergioagreda21@outlook.com"],  # Change to manager's email
+        recipients=["claudia@parlamento.com.bo"],  # Change to manager's email
         body=body,
         subtype="plain"
     )
